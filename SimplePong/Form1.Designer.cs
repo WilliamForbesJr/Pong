70adf98d40ce0c4099c763eb788a1e11b3ea1a31
Will <forbes8963@gmail.com>
2020-04-17 19:14:07 -0400

    first commit

---
+++ SimplePong/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace SimplePong
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Ball = new System.Windows.Forms.PictureBox();
+            this.Player = new System.Windows.Forms.PictureBox();
+            this.Computer = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Computer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Ball
+            // 
+            this.Ball.Image = ((System.Drawing.Image)(resources.GetObject("Ball.Image")));
+            this.Ball.Location = new System.Drawing.Point(0, 0);
+            this.Ball.Name = "Ball";
+            this.Ball.Size = new System.Drawing.Size(25, 25);
+            this.Ball.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.Ball.TabIndex = 0;
+            this.Ball.TabStop = false;
+            // 
+            // Player
+            // 
+            this.Player.BackColor = System.Drawing.Color.Yellow;
+            this.Player.Location = new System.Drawing.Point(10, 142);
+            this.Player.Name = "Player";
+            this.Player.Size = new System.Drawing.Size(27, 127);
+            this.Player.TabIndex = 1;
+            this.Player.TabStop = false;
+            // 
+            // Computer
+            // 
+            this.Computer.BackColor = System.Drawing.Color.Fuchsia;
+            this.Computer.Location = new System.Drawing.Point(750, 142);
+            this.Computer.Name = "Computer";
+            this.Computer.Size = new System.Drawing.Size(27, 127);
+            this.Computer.TabIndex = 1;
+            this.Computer.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.Computer);
+            this.Controls.Add(this.Player);
+            this.Controls.Add(this.Ball);
+            this.Name = "Form1";
+            this.Text = "Simple Pong";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Computer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Ball;
+        private System.Windows.Forms.PictureBox Player;
+        private System.Windows.Forms.PictureBox Computer;
+    }
+}
+
---
+++ SimplePong/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimplePong
+{
+    public partial class Form1 : Form
+    {
+
+        const int DISTANCE_FROM_EDGE = 10;      // distance of comp paddle from playing field edge
+
+        
+        bool GoUp, GoDown;                      // Keyboard up and down status
+
+        const int PLAYER_PADDLE_SPEED = 8;      // player speed (pixels)
+
+        const int TIME_DELAY = 2;              // game speed
+
+        // x and y movement constants
+        const int SPEED_X = 2;
+        const int SPEED_Y = 2;
+
+        // vertical and horizontal movement of ball based on speed consts
+        int MoveX = SPEED_X;
+        int MoveY = SPEED_Y;
+
+        public void GameLoop()
+        {
+            /* Main game loop, executing every 10ms from start
+             * this: refers to current form class
+             * created: boolean, while while loop tests for true is form class is running
+             */
+            while (this.Created)
+            {
+                moveBall();
+                detectCollision();
+                movePaddle();            
+                Refresh();              // repaint form
+                // Processes all events in thread queue so program animation doesn't stop during refresh
+                
+                // pause foreground or program thread for time_delay
+                System.Threading.Thread.Sleep(TIME_DELAY);
+            }
+        }
+        
+        // Reverse direction upon ball/paddle collision
+        private void detectCollision()
+        {
+            if (Ball.Bounds.IntersectsWith(Player.Bounds) || Ball.Bounds.IntersectsWith(Computer.Bounds))
+            {
+                MoveX = -MoveX; // reverse direction
+            }
+        }
+
+        private void movePaddle()
+        {
+
+            // Computer logic. Need to abstract it and clean up
+
+            // adjust computer paddle on screen resize
+            Computer.Left = ClientSize.Width - (Computer.Width + DISTANCE_FROM_EDGE);
+
+            // Move computer paddle up/down
+            int ComputerPaddleSpeed = 5;            // computer paddle speed (pixels)
+
+            if (Computer.Bottom < Ball.Bottom && Ball.Left > (ClientSize.Width / 2))
+            {
+                Computer.Top += ComputerPaddleSpeed;
+            }
+
+            else if (Computer.Top > Ball.Top && Ball.Left > (ClientSize.Width / 2))
+            {
+                Computer.Top -= ComputerPaddleSpeed;
+            }
+
+            // Player Logic
+
+            // up arrow and not at top of form
+            if (GoUp && Player.Top > 0)
+            {
+                Player.Top -= PLAYER_PADDLE_SPEED; // move up
+            }
+
+            // down arrow and not at bottom of form
+            if (GoDown && Player.Bottom < ClientSize.Height)
+            {
+                Player.Top += PLAYER_PADDLE_SPEED; //move down
+            }
+        }
+
+        // logic to move ball on playing field
+        private void moveBall()
+        {
+            // move ball on x and y by adding integers
+            Ball.Left = Ball.Left + MoveX;
+            Ball.Top = Ball.Top + MoveY;
+
+            if (Ball.Left < 0 || Ball.Left + Ball.Width > ClientSize.Width)
+            {
+                MoveX = -MoveX;
+            }
+            if (Ball.Top < 0 || Ball.Top + Ball.Height > ClientSize.Height)
+            {
+                MoveY = -MoveY;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // key press logic for down press on up/down arrows
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                GoUp = true;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                GoDown = true;
+            }
+        }
+
+        // keypress logic for release on up/down arrows
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                GoUp = false;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                GoDown = false;
+            }
+        }
+    }
+}
