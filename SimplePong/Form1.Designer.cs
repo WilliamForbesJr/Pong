785b511602adc0d7d8350d2820ba001e2b1656f1
Will <forbes8963@gmail.com>
2020-04-30 12:14:56 -0400

    add scoring feature

--- SimplePong/Form1.Designer.cs
+++ SimplePong/Form1.Designer.cs
@@ -32,6 +32,8 @@
             this.Ball = new System.Windows.Forms.PictureBox();
             this.Player = new System.Windows.Forms.PictureBox();
             this.Computer = new System.Windows.Forms.PictureBox();
+            this.lblScoreComp = new System.Windows.Forms.Label();
+            this.lblScorePlayer = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.Ball)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.Player)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.Computer)).BeginInit();
@@ -50,20 +52,40 @@
             // Player
             // 
             this.Player.BackColor = System.Drawing.Color.Yellow;
-            this.Player.Location = new System.Drawing.Point(10, 142);
+            this.Player.Location = new System.Drawing.Point(16, 132);
             this.Player.Name = "Player";
-            this.Player.Size = new System.Drawing.Size(27, 127);
+            this.Player.Size = new System.Drawing.Size(10, 127);
             this.Player.TabIndex = 1;
             this.Player.TabStop = false;
             // 
             // Computer
             // 
             this.Computer.BackColor = System.Drawing.Color.Fuchsia;
-            this.Computer.Location = new System.Drawing.Point(750, 142);
+            this.Computer.Location = new System.Drawing.Point(764, 132);
             this.Computer.Name = "Computer";
-            this.Computer.Size = new System.Drawing.Size(27, 127);
+            this.Computer.Size = new System.Drawing.Size(10, 127);
             this.Computer.TabIndex = 1;
             this.Computer.TabStop = false;
+            // 
+            // lblScoreComp
+            // 
+            this.lblScoreComp.AutoSize = true;
+            this.lblScoreComp.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScoreComp.Location = new System.Drawing.Point(667, 11);
+            this.lblScoreComp.Name = "lblScoreComp";
+            this.lblScoreComp.Size = new System.Drawing.Size(107, 20);
+            this.lblScoreComp.TabIndex = 2;
+            this.lblScoreComp.Text = "Computer: 0";
+            // 
+            // lblScorePlayer
+            // 
+            this.lblScorePlayer.AutoSize = true;
+            this.lblScorePlayer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScorePlayer.Location = new System.Drawing.Point(12, 9);
+            this.lblScorePlayer.Name = "lblScorePlayer";
+            this.lblScorePlayer.Size = new System.Drawing.Size(78, 20);
+            this.lblScorePlayer.TabIndex = 2;
+            this.lblScorePlayer.Text = "Player: 0";
             // 
             // Form1
             // 
@@ -71,6 +93,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.lblScorePlayer);
+            this.Controls.Add(this.lblScoreComp);
             this.Controls.Add(this.Computer);
             this.Controls.Add(this.Player);
             this.Controls.Add(this.Ball);
@@ -91,6 +115,8 @@
         private System.Windows.Forms.PictureBox Ball;
         private System.Windows.Forms.PictureBox Player;
         private System.Windows.Forms.PictureBox Computer;
+        private System.Windows.Forms.Label lblScoreComp;
+        private System.Windows.Forms.Label lblScorePlayer;
     }
 }
 
--- SimplePong/Form1.cs
+++ SimplePong/Form1.cs
@@ -12,15 +12,21 @@ namespace SimplePong
 {
     public partial class Form1 : Form
     {
+        const int WIN = 10;                     // Winning Score
+        const int INCREASE_SPEED = 1;           // speed increase factor
+
+        //starting scores
+        int playerScore = 0;
+        int computerScore = 0;
 
         const int DISTANCE_FROM_EDGE = 10;      // distance of comp paddle from playing field edge
 
-        
         bool GoUp, GoDown;                      // Keyboard up and down status
 
         const int PLAYER_PADDLE_SPEED = 8;      // player speed (pixels)
+        int ComputerPaddleSpeed = 6;            // computer paddle speed (pixels)
 
-        const int TIME_DELAY = 2;              // game speed
+        const int TIME_DELAY = 10;              // game speed
 
         // x and y movement constants
         const int SPEED_X = 2;
@@ -39,21 +45,89 @@ namespace SimplePong
             while (this.Created)
             {
                 moveBall();
+                keepScore();
                 detectCollision();
                 movePaddle();            
                 Refresh();              // repaint form
                 // Processes all events in thread queue so program animation doesn't stop during refresh
-                
+                Application.DoEvents();
                 // pause foreground or program thread for time_delay
                 System.Threading.Thread.Sleep(TIME_DELAY);
+            }
+        }
+
+        private void keepScore()
+        {
+            if (Ball.Left < 0)
+            {
+                Ball.Left = ClientSize.Width / 2;
+                MoveX = -MoveX;
+                MoveX += INCREASE_SPEED;
+
+                if (MoveY < 0)
+                {
+                    MoveY -= INCREASE_SPEED;
+                }
+                else
+                {
+                    MoveY += INCREASE_SPEED;
+                }
+                computerScore++;
+            }
+
+            // computer misses player scores
+            if (Ball.Left + Ball.Width > ClientSize.Width)
+            {
+                Ball.Left = ClientSize.Width / 2;
+                MoveX = -MoveX;
+                MoveX -= INCREASE_SPEED;
+
+                if (MoveY < 0)
+                {
+                    MoveY -= INCREASE_SPEED;
+                }
+                else
+                {
+                    MoveY += INCREASE_SPEED;
+                }
+                playerScore++;
+            }
+
+            lblScorePlayer.Text = $"Player: {playerScore.ToString()}";
+            lblScoreComp.Text = $"Computer: {computerScore.ToString()}";
+
+            if (playerScore >= WIN)
+            {
+                MessageBox.Show("You Won!");
+                playerScore = 0;
+                computerScore = 0;
+            }
+            if (computerScore >= WIN)
+            {
+                MessageBox.Show("The Computer Won!");
+                playerScore = 0;
+                computerScore = 0;
             }
         }
         
         // Reverse direction upon ball/paddle collision
         private void detectCollision()
         {
-            if (Ball.Bounds.IntersectsWith(Player.Bounds) || Ball.Bounds.IntersectsWith(Computer.Bounds))
+            // flags added to determine which player hit ball last.
+            // This avoids collision detection but where ball gets stuck in x/y loop inside paddle
+            bool compHitLast = false;
+            bool playerHitLast = false;
+
+            if (Ball.Bounds.IntersectsWith(Player.Bounds) && !playerHitLast)
             {
+                compHitLast = false;
+                playerHitLast = true;
+                MoveX = -MoveX; // reverse direction
+            }
+            if (Ball.Bounds.IntersectsWith(Computer.Bounds) && !compHitLast)
+            {
+                compHitLast = true;
+                playerHitLast = false;
                 MoveX = -MoveX; // reverse direction
             }
         }
@@ -67,14 +141,13 @@ namespace SimplePong
             Computer.Left = ClientSize.Width - (Computer.Width + DISTANCE_FROM_EDGE);
 
             // Move computer paddle up/down
-            int ComputerPaddleSpeed = 5;            // computer paddle speed (pixels)
 
-            if (Computer.Bottom < Ball.Bottom && Ball.Left > (ClientSize.Width / 2))
+            if (Computer.Bottom < Ball.Bottom && Ball.Left > (ClientSize.Width / 1.4))
             {
                 Computer.Top += ComputerPaddleSpeed;
             }
 
-            else if (Computer.Top > Ball.Top && Ball.Left > (ClientSize.Width / 2))
+            else if (Computer.Top > Ball.Top && Ball.Left > (ClientSize.Width / 1.4))
             {
                 Computer.Top -= ComputerPaddleSpeed;
             }
@@ -101,11 +174,7 @@ namespace SimplePong
             Ball.Left = Ball.Left + MoveX;
             Ball.Top = Ball.Top + MoveY;
 
-            if (Ball.Left < 0 || Ball.Left + Ball.Width > ClientSize.Width)
-            {
-                MoveX = -MoveX;
-            }
-            if (Ball.Top < 0 || Ball.Top + Ball.Height > ClientSize.Height)
+            if (Ball.Top <= 0 || Ball.Top + Ball.Height >= ClientSize.Height)
             {
                 MoveY = -MoveY;
             }
